5961de5fc5888fb83b26448a6186aa446dadcc3d
yukitaka-sakai <aku.kaya.kulkas.yukitaka.ski@gmail.com>
2020-08-06 12:34:43 +0000

    ログインフォーム作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get     '/login', to: 'sessions#new'
+  post    '/login', to: 'sessions#create'
+  delete  '/login', to: 'sessions#destroy'
+  
   root 'static_pages#top'
   get  '/signup', to:'users#new'
+  
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
