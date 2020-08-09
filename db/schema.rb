84b2fe13fc3229356148923c6ee95fcc0ca4d09b
yukitaka-sakai <aku.kaya.kulkas.yukitaka.ski@gmail.com>
2020-08-09 13:40:23 +0000

    基本時間表示

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200809103724) do
+ActiveRecord::Schema.define(version: 20200809133226) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -21,6 +21,9 @@ ActiveRecord::Schema.define(version: 20200809103724) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "department"
+    t.datetime "basic_time", default: "2020-08-08 23:00:00"
+    t.datetime "end_of_worktime", default: "2020-08-09 08:30:00"
+    t.datetime "work_time", default: "2020-08-08 22:30:00"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
