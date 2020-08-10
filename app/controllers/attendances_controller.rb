b4f75544c8f885bda4e05ec45b851f816d4c0400
yukitaka-sakai <aku.kaya.kulkas.yukitaka.ski@gmail.com>
2020-08-10 10:55:48 +0000

    退勤ボタン実装

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,4 +1,5 @@
 class AttendancesController < ApplicationController
+  UPDATE_ERROR_MSG = "勤怠登録に失敗しました。やり直して下さい。"
 
   def update
     @user = User.find(params[:user_id])
@@ -8,7 +9,13 @@ class AttendancesController < ApplicationController
       if @attendance.update_attributes(started_at: Time.current.change(sec: 0))
         flash[:info] = "goodmorningですな"
       else
-        flash[:danger] = "登録失敗やり直せ！"
+        flash[:danger] = UPDATE_ERROR_MSG
+      end
+    elsif @attendance.finished_at.nil?
+      if @attendance.update_attributes(finished_at: Time.current.change(sec: 0))
+        flash[:info] = "おつかレーション"
+      else
+        flash[:danger] = UPDATE_ERROR_MSG
       end
     end
     redirect_to @user
--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -1,2 +1,12 @@
 module AttendancesHelper
-end
+  
+  def attendance_state(attendance) #ボタンのコードを一つにしてボタンに表示されるテキストの条件を決める。
+    # 受けとったAttendanceオブジェクトが当日と一致するか評価する
+    if Date.current == attendance.worked_on
+      return "出勤" if attendance.started_at.nil?
+      return "退勤" if attendance.started_at.present? && attendance.finished_at.nil?
+    end
+    # どれにも当てはまらない場合falseをかえす
+    false
+  end
+end 
