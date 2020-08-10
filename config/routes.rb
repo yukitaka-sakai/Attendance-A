2ef86cd0d140eb2ebb0c849450080473e38554ee
yukitaka-sakai <aku.kaya.kulkas.yukitaka.ski@gmail.com>
2020-08-10 09:54:55 +0000

    attendanceルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   root 'static_pages#top'
   get  '/signup', to:'users#new'
   
-  resources :users
+  resources :users do
+    resources :attendances, only: :update
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
