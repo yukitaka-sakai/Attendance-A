27b7c304635a841371f215e11097f4f80dde5f76
yukitaka-sakai <aku.kaya.kulkas.yukitaka.ski@gmail.com>
2020-08-10 11:45:21 +0000

    在社時間出力

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -9,4 +9,9 @@ module AttendancesHelper
     # どれにも当てはまらない場合falseをかえす
     false
   end
+  
+  # 出社時間と退社時間を受け取り、在社時間を計算して返す。
+  def working_times(start, finish)
+    format("%.2f", (((finish - start) / 60) / 60.0))
+  end
 end 
