f609d346152531f207289c4e9bb2b8d901041ef3
yukitaka-sakai <aku.kaya.kulkas.yukitaka.ski@gmail.com>
2020-08-10 01:52:56 +0000

    曜日追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   #モジュールを読み込みヘルパーで定義したメソッドがどのコントローラーでも使える様になる
   include SessionsHelper
+  
+  $day_of_the_week = %w{日 月 火 水 木 金 土}
 end
