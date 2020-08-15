b83f7ce9f825f19feb2b784907f96fb9278cd37f
yukitaka-sakai <aku.kaya.kulkas.yukitaka.ski@gmail.com>
2020-08-15 07:40:03 +0000

    edit_one_month

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -3,7 +3,7 @@ class AttendancesController < ApplicationController
   before_action :logged_in_user, only: [:updafte, :edit_ont_month]
   before_action :set_one_month, only: :edit_one_month
   
-  UPDATE_ERROR_MSG = "勤怠登録に失敗しました。やり直して下さい。"
+  UPDATE_ERROR_MSG = "勤怠登録に失���しました。やり直して下さい。"
 
   def update
     @user = User.find(params[:user_id])
@@ -27,4 +27,24 @@ class AttendancesController < ApplicationController
   
   def edit_one_month
   end
+  
+  def update_one_month
+    ActiveRecord::Base.transaction do  # トランザクションを開始する
+      attendances_params.each do |id, item|
+        attendance = Attendance.find(id)
+        attendance.update_attributes!(item)
+      end
+    end
+    flash[:success] = "1ヶ月分の勤怠情報を更新しました。"
+    redirect_to user_url(date: params[:date])
+  rescue ActiveRecord::RecordInvalid
+    flash[:danger] = "無効な入力データがあった為、更新をキャンセルt更新をキャンセルしました。"
+    redirect_to attendances_edit_one_month_user_url(date: params[:date])
+  end
+    
+  private
+    # 1ヶ月分の勤怠情報を扱います。
+    def attendances_params
+      params.require(:user).permit(attendances: [:started_at, :finished_at, :note])[:attendances]
+    end
 end
--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -4,9 +4,19 @@ class Attendance < ApplicationRecord
   validates   :worked_on, presence: true
   validates   :note, length: { maximum: 50 }
   
+  
+  # 出社時間がない場合、退勤時間は無効
   validate :finished_at_is_invalid_without_a_started_at
+  # 出社。退社時間どちらも存在する場合、出社時間より早い退社時間は無効
+  validate :started_at_than_finished_at_fast_if_invalid
   
   def finished_at_is_invalid_without_a_started_at
     errors.add(:started_at, "が必要です") if started_at.blank? && finished_at.present?
+  end
+  
+  def started_at_than_finished_at_fast_if_invalid
+    if started_at.present? && finished_at.present?
+      errors.add(:started_at, "より早い退勤時間は無効です") if started_at > finished_at
+    end 
   end
 end
