ca00d20cfc6a1c389fe242cc5458bf442023a90b
yukitaka-sakai <aku.kaya.kulkas.yukitaka.ski@gmail.com>
2020-08-30 16:59:28 +0000

    csv　カウント修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,8 @@ class UsersController < ApplicationController
         format.html 
         format.csv do
             send_data render_to_string, 
-            filename: "#{@user.name}.csv", type: :csv #csv用の処理を書く
+            filename: "#{@user.name}-#{@first_day.year}_#{@first_day.month}.csv", type: :csv #csv用の処理を書く
+            
         end
       end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -20,20 +20,23 @@ class User < ApplicationRecord
 
 
   def self.import(file)
+    num = 0
     CSV.foreach(file.path, headers: true,) do |row|
       # IDが見つかれば、レコードを呼び出し、見つかれなければ、新しく作成
       user = find_by(id: row["id"]) || new
       # CSVからデータを取得し、設定する
       user.attributes = row.to_hash.slice(*updatable_attributes)
-      # debugger
+      
       # 保存する
       user.save
+      num += 1
     end
+    num
   end
 
   # 更新を許可するカラムを定義
   def self.updatable_attributes
-    ["id", "name", "email" ,"affiliation", "employee_number", "uid", "basic_work_time", "designated_work_start_time", "designated_work_end_time", "superior", "admin", "password"]
+    ["id", "name", "email" ,"affiliation", "uid", "employee_number", "basic_work_time", "designated_work_start_time", "designated_work_end_time", "superior", "admin", "password"]
   end
 
 
