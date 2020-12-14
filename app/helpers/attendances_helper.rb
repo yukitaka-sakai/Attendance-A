09547d2313aabfdf7669a3d190c7a3d0eb5364c4
yukitaka-sakai <aku.kaya.kulkas.yukitaka.ski@gmail.com>
2020-12-14 08:06:41 +0000

    時間外時間をshow,approvalshow,overtime_approvalに表示

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -20,19 +20,20 @@ module AttendancesHelper
     end
   end
   
-  def overtimes(overtime_finished_at, designated_work_end_time, overtime_next_day)
-    if overtime_next_day == "1"
-      format("%.2f", ((overtime_finished_at.hour - designated_work_end_time.hour) + ((overtime_finished_at.min - designated_work_end_time.min) / 60.0) + 24))
+  # メソッド 名（1,2,3)の順番で引数を呼び出す。引数はカラム名は使わない。
+  def overtimes(overtimefinish, designated_work_end_time, next_day)
+    if next_day == "1"
+      format("%.2f", ((overtimefinish.hour - designated_work_end_time.hour) + ((overtimefinish.min - designated_work_end_time.min) / 60.0) + 24))
     else
-      format("%.2f", ((overtime_finished_at.hour - designated_work_end_time.hour) + ((overtime_finished_at.min - designated_work_end_time.min) / 60.0)))
+      format("%.2f", ((overtimefinish.hour - designated_work_end_time.hour) + ((overtimefinish.min - designated_work_end_time.min) / 60.0)))
     end
   end
 
-  def format_hour(time)
+  def format_hour(time) #時間表示の「時間部分」を２桁（００）で表示する。
     format("%02d", time.hour)
   end
   
-  def format_min(time)
-    format("%02d", ((time.min / 15) * 15))
+  def format_min(time) #時間表示び「分部分」を２桁（００）で表示する。
+    format("%02d", ((time.min / 15) * 15)) #time.minを15で割り、%02bで自動的に小数点は消え、15を掛け戻すことで15分で丸まる
   end
 end 
