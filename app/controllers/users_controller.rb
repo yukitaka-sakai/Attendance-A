57e26f49e93637ef1ee875c0ae64e7cdc41ae849
yukitaka-sakai <aku.kaya.kulkas.yukitaka.ski@gmail.com>
2021-03-11 22:29:33 +0000

    画面遷移バリデーション編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :set_user,       only: [:show, :edit, :update, :destroy, :approval_show]
   before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy, :approval_show]
   before_action :correct_user,   only: [:show, :edit, :update, :destroy]
-  before_action :admin_user,     only: [:destroy, :edit, :update]
+  before_action :admin_user,     only: [:destroy, :edit, :update, :index, :employee_index]
   before_action :set_one_month,  only: [:show, :approval_show]
   before_action :set_month,  only: :show
   
